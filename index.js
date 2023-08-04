dfa3a3f0ed1cacb90a6082726e930e8055ff4d4b
Joshua Linang <jlinang06@gmail.com>
2023-08-04 15:59:15 -0400

    challenge 5 filters

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function addGamesToPage(games) {
 
 
     // set inner HTML using template literal
-    raisedCard.innerHTML = `<p> ${totalpledged.toLocaleString('en-US')} </p>`;
+    raisedCard.innerHTML = `<p> $${totalpledged.toLocaleString('en-US')} </p>`;
 
     // grab number of games card and set its inner HTML
     const gamesCard = document.getElementById("num-games");
@@ -110,10 +110,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let unfundedlist = GAMES_JSON.filter((game) =>{
+        return game.goal > game.pledged;
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedlist);
 }
 
 // show only games that are fully funded
@@ -121,17 +123,21 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let fundedFilter = GAMES_JSON.filter( (game) =>{
+        return game.pledged > game.goal || game.pledged == game.goal;
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedFilter);
 }
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
 
+
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -141,6 +147,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener('click', filterUnfundedOnly);
+fundedBtn.addEventListener('click', filterFundedOnly);
+allBtn.addEventListener('click', showAllGames); 
 
 
 /*************************************************************************************
